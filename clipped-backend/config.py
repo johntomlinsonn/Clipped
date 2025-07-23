2b759727602190cc04684113bef110248e14b4a9
John Tomlinson <tomlinson.johnq@gmail.com>
2025-07-22 20:22:17 -0500

    changes to get docker working

--- clipped-backend/config.py
+++ clipped-backend/config.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from pydantic import BaseSettings, Field, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
 load_dotenv()
 

ae197856cc49c487d28eebdab3aa6d8b63f5d594
John Tomlinson <tomlinson.johnq@gmail.com>
2025-07-22 20:05:40 -0500

    docker setup

--- clipped-backend/config.py
+++ clipped-backend/config.py
@@ -1,16 +1,15 @@
 import os
-from dotenv import load_dotenv
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
+from dotenv import load_dotenv
+from pydantic import BaseSettings, Field, SettingsConfigDict
 
 load_dotenv()
 
 class Settings(BaseSettings):
-    host: str = os.getenv("FASTAPI_HOST", "127.0.0.1")
-    port: int = int(os.getenv("FASTAPI_PORT", 8000))
-    tmp_dir: Path = Path(os.getenv("TMP_DIR", "./tmp"))
-
-    storage_dir: Path = Path(os.getenv("STORAGE_DIR", "./storage"))
+    host: str = Field("127.0.0.1", env="FASTAPI_HOST")
+    port: int = Field(8000, env="FASTAPI_PORT")
+    tmp_dir: Path = Field(Path("./tmp"), env="TMP_DIR")
+    storage_dir: Path = Field(Path("./storage"), env="STORAGE_DIR")
 
     cerebras_api_key: str | None = None
 
